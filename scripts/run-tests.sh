be688a8e121595aa3ba0ac75462af75a0f7c0861
Jerin Philip <jerinphilip@live.in>
2023-06-16 21:45:45 +0530

    Update scripts

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,8 +2,15 @@
 
 FILES=$(find test-units -iname "*.b")
 TIMEOUT=5s
+FLATB_EXECUTABLE=./build/flatb
+
 
 for file in ${FILES[@]}; do
-  echo ./build/src/fb -i $file;
-  timeout $TIMEOUT ./build/src/fb -i $file;
+  echo $FLATB_EXECUTABLE -c $file;
+  timeout $TIMEOUT $FLATB_EXECUTABLE -c $file > /dev/null;
+done;
+
+for file in ${FILES[@]}; do
+  echo $FLATB_EXECUTABLE -i $file;
+  timeout $TIMEOUT $FLATB_EXECUTABLE -i $file > /dev/null;
 done;
