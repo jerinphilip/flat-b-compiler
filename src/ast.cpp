6353a03187a97b683be0a745b64a8414b66687b2
Jerin Philip <jerinphilip@live.in>
2023-06-27 00:00:52 +0530

    Proper visitor pattern

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,84 +1,32 @@
 #include "ast.h"
 
 #include "compiler.h"
-#include "interpreter.h"
 #include "formatter.h"
+#include "interpreter.h"
 #include "visitor.h"
 
 /* PPRINTER */
-void ast::Program::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Declarations::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Id::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::IdArrayAccess::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Expr::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Block::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Statement::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Assign::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::While::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::If::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::For::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Print::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::TypedIds::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Integer::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::BinOp::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::NoOp::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Goto::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::IdDef::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::IdRef::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::IdArrayDef::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::IdArrayRef::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Literal::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Read::accept(visitor::Formatter *p) { p->visit(this); }
-void ast::Labelled::accept(visitor::Formatter *p) { p->visit(this); }
-
-/* INTERPRETER */
-void ast::Program::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Declarations::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Id::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::IdArrayAccess::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Expr::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Block::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Statement::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Assign::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::While::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::If::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::For::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Print::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::TypedIds::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Integer::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::BinOp::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::NoOp::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Goto::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::IdDef::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::IdRef::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::IdArrayDef::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::IdArrayRef::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Literal::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Read::accept(visitor::Interpreter *p) { p->visit(this); }
-void ast::Labelled::accept(visitor::Interpreter *p) { p->visit(this); }
-
-/* COMPILER */
-void ast::Program::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Declarations::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Id::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::IdArrayAccess::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Expr::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Block::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Statement::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Assign::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::While::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::If::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::For::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Print::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::TypedIds::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Integer::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::BinOp::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::NoOp::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Goto::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::IdDef::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::IdRef::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::IdArrayDef::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::IdArrayRef::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Literal::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Read::accept(visitor::Compiler *p) { p->visit(this); }
-void ast::Labelled::accept(visitor::Compiler *p) { p->visit(this); }
+void ast::Program::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Declarations::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Id::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::IdArrayAccess::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Expr::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Block::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Statement::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Assign::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::While::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::If::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::For::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Print::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::TypedIds::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Integer::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::BinOp::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::NoOp::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Goto::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::IdDef::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::IdRef::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::IdArrayDef::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::IdArrayRef::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Literal::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Read::accept(visitor::Visitor *p) { p->visit(this); }
+void ast::Labelled::accept(visitor::Visitor *p) { p->visit(this); }
--- src/ast.h
+++ src/ast.h
@@ -10,9 +10,7 @@ enum class Op { add, sub, mul, quot, lt, gt, le, ge, eq };
 
 // Forward declarations.
 namespace visitor {
-struct Formatter;
-struct Interpreter;
-struct Compiler;
+struct Visitor;
 }  // namespace visitor
 
 namespace ast {
@@ -34,9 +32,7 @@ struct Print;
 struct TypedIds;
 
 struct Node {
-  virtual void accept(visitor::Formatter *) = 0;
-  virtual void accept(visitor::Interpreter *) = 0;
-  virtual void accept(visitor::Compiler *) = 0;
+  virtual void accept(visitor::Visitor *) = 0;
 
   virtual ~Node() = default;
 };
@@ -45,9 +41,7 @@ struct Program : public Node {
   Declarations *declarations;
   Block *block;
   Program(Declarations *d, Block *c) : declarations(d), block(c) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 /* declarations */
@@ -56,16 +50,12 @@ struct Declarations : public Node {
   std::vector<TypedIds *> *typed_ids;
   Declarations(std::vector<TypedIds *> *v) : typed_ids(v) {}
 
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 /* Auxiliary types for declations and onward */
 struct Expr : public Node {
-  virtual void accept(visitor::Formatter *p);
-  virtual void accept(visitor::Interpreter *p);
-  virtual void accept(visitor::Compiler *p);
+  virtual void accept(visitor::Visitor *p);
   virtual ~Expr() = default;
 };
 
@@ -73,9 +63,7 @@ struct Id : public Expr {
   std::string name;
   Id(std::string name) : name(name) {}
 
-  virtual void accept(visitor::Formatter *p);
-  virtual void accept(visitor::Interpreter *p);
-  virtual void accept(visitor::Compiler *p);
+  virtual void accept(visitor::Visitor *p);
 
   virtual ~Id() = default;
 };
@@ -85,9 +73,7 @@ struct IdArrayAccess : public Id {
 
   IdArrayAccess(std::string name, Expr *subscript)
       : Id(name), subscript(subscript) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct IdRef : public Node {
@@ -95,9 +81,7 @@ struct IdRef : public Node {
   IdRef(std::string s) : name(s) {
     // cerr << "Initializing: " << name << endl;
   }
-  virtual void accept(visitor::Formatter *p);
-  virtual void accept(visitor::Interpreter *p);
-  virtual void accept(visitor::Compiler *p);
+  virtual void accept(visitor::Visitor *p);
   Id *id() { return new Id(name); }
   virtual ~IdRef() = default;
 };
@@ -105,43 +89,33 @@ struct IdRef : public Node {
 struct IdArrayRef : public IdRef {
   Expr *subscript;
   IdArrayRef(std::string s, Expr *e) : IdRef(s), subscript(e) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct IdDef : public Node {
   std::string name;
   IdDef(std::string s) : name(s) {}
-  virtual void accept(visitor::Formatter *p);
-  virtual void accept(visitor::Interpreter *p);
-  virtual void accept(visitor::Compiler *p);
+  virtual void accept(visitor::Visitor *p);
   virtual ~IdDef() = default;
 };
 
 struct IdArrayDef : public IdDef {
   int size;
   IdArrayDef(std::string s, int sz) : IdDef(s), size(sz) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct TypedIds : public Node {
   FlatBType type;
   std::vector<IdDef *> *id_defs;
   TypedIds(FlatBType d, std::vector<IdDef *> *v) : type(d), id_defs(v) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct Integer : public Expr {
   int value;
   Integer(int v) : value(v) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct BinOp : public Expr {
@@ -150,15 +124,11 @@ struct BinOp : public Expr {
   Expr *right;
 
   BinOp(Op op, Expr *l, Expr *r) : op(op), left(l), right(r) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct Statement : public Node {
-  virtual void accept(visitor::Formatter *p);
-  virtual void accept(visitor::Interpreter *p);
-  virtual void accept(visitor::Compiler *p);
+  virtual void accept(visitor::Visitor *p);
   virtual ~Statement() = default;
 };
 
@@ -166,18 +136,14 @@ struct Statement : public Node {
 struct Block : public Node {
   std::vector<Statement *> *statements;
   Block(std::vector<Statement *> *s) : statements(s) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct Labelled : public Statement {
   std::string label;
   Block *block;
   Labelled(std::string label, Block *s) : label(label), block(s) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct Assign : public Statement {
@@ -186,18 +152,14 @@ struct Assign : public Statement {
 
   Assign(IdRef *ref, Expr *e) : ref(ref), tree(e) {}
 
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct While : public Statement {
   Expr *condition;
   Block *block;
   While(Expr *c, Block *b) : condition(c), block(b) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct If : public Statement {
@@ -206,9 +168,7 @@ struct If : public Statement {
   Block *otherwise;
   If(Expr *c, Block *b, Block *o = NULL)
       : condition(c), block(b), otherwise(o) {}
-  void accept(visitor::Formatter *p) final;
-  void accept(visitor::Interpreter *p) final;
-  void accept(visitor::Compiler *p) final;
+  void accept(visitor::Visitor *p) final;
 };
 
 struct For : public Statement {
@@ -219,18 +179,14 @@ struct For : public Statement {
 
   For(Assign *init, Expr *delta, Expr *end, Block *b)
       : init(init), step(delta), end(end), body(b) {}
-  void accept(visitor::Formatter *p) final;
-  void accept(visitor::Interpreter *p) final;
-  void accept(visitor::Compiler *p) final;
+  void accept(visitor::Visitor *p) final;
 };
 
 struct Goto : public Statement {
   Expr *condition;
   std::string label;
   Goto(std::string label, Expr *e = NULL) : label(label), condition(e) {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct Print : public Statement {
@@ -240,32 +196,24 @@ struct Print : public Statement {
   Print(std::vector<ast::Expr *> *args, bool newline)
       : args(args), newline(newline) {}
 
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct NoOp : public Statement {
   NoOp() {}
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct Literal : public Expr {
   std::string value;
   Literal(std::string s) { value = s.substr(1, s.size() - 2); }
-  void accept(visitor::Formatter *p);
-  void accept(visitor::Interpreter *p);
-  void accept(visitor::Compiler *p);
+  void accept(visitor::Visitor *p);
 };
 
 struct Read : public Statement {
   IdRef *var;
 
   Read(IdRef *var) : var(var) {}
-  void accept(visitor::Formatter *p) final;
-  void accept(visitor::Interpreter *p) final;
-  void accept(visitor::Compiler *p) final;
+  void accept(visitor::Visitor *p) final;
 };
 }  // namespace ast
