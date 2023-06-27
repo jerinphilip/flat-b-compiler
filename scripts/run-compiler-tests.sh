0dc4217734e6e82e9968e3112d2e6b0de828d9b8
Jerin Philip <jerinphilip@live.in>
2023-06-27 13:05:37 +0530

    Add compilation into scripts, some still not working

--- scripts/run-compiler-tests.sh
+++ scripts/run-compiler-tests.sh
@@ -1,12 +1,16 @@
-
 #!/bin/bash
 
 FILES=$(find test-units -iname "*.b")
 TIMEOUT=5s
 FLATB_EXECUTABLE=./build/flatb
 
+set -x
+
 # The following tests are disabled until I can fix the LLVM parts of it.
 for file in ${FILES[@]}; do
-  echo $FLATB_EXECUTABLE -c $file;
-  timeout $TIMEOUT $FLATB_EXECUTABLE -c $file > /dev/null;
-done;
+  echo $FLATB_EXECUTABLE -c $file
+  timeout $TIMEOUT $FLATB_EXECUTABLE -c $file > $file.ll
+  llc -O3 $file.ll -o $file.s
+  gcc $file.s -o a.out -static
+  ./a.out
+done
